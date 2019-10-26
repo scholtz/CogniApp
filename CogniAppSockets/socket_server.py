78352ad94d3a5101a87c7a5661452453851783dd
Martin Kubajda <m.kubajda@kendaxa.com>
2019-10-26 23:05:06 +0200

    eeg_event post and get as separete events, host changed to 0.0.0.0

--- CogniAppSockets/socket_server.py
+++ CogniAppSockets/socket_server.py
@@ -18,10 +18,27 @@ eeg_events = [
 session_ids = []
 
 
-@socketio.on('eeg_event')
-def handle_eeg_event(json):
+@socketio.on('eeg_event_post')
+def handle_eeg_post_event(json):
 	eeg_events.append(json)
 	print('received eeg_event: ' + str(json))
+
+
+@socketio.on('eeg_event_get')
+def handle_eeg_get_event(session_id):
+	events = []
+	eeg_events_copy = eeg_events
+	for eeg_event in eeg_events_copy:
+		if session_id == eeg_event['sesssion_id']:
+			events.append(eeg_event)
+			eeg_events.remove(eeg_event)
+		else:
+			continue
+
+	if events:
+		return events
+	else:
+		return False
 
 
 @socketio.on('new_game_event')
@@ -41,4 +58,4 @@ def handle_end_game_event(json):
 
 
 if __name__ == '__main__':
-	socketio.run(app)
+	socketio.run(app, host='0.0.0.0', port=5000)
